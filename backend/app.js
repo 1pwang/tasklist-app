5ace508d245797439e177a39a732576a25fd5f3a
Pauline Wangunyu <paulinewangunyu@Paulines-MacBook-Air.local>
2024-02-14 17:09:08 +0000

    Connect to remote mongodb database

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const url = 'mongodb+srv://paulinewang222:nfzaC54scBtUqt30@cluster0.mywzxrj.mongodb.net/tasklist?retryWrites=true&w=majority'; // Fixed MongoDB connection URL
+const port = 3000;
+
+mongoose.connect(url, {}) 
+  .then(result => console.log('Connected'))
+  .catch(err => console.error('Error', err));
+
+app.listen(port, () => {
+  console.log('Server running on port', port);
+});
